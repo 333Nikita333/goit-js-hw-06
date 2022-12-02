35f2fe2178bd26a6e6c58bce684acc59d0b42845
333Nikita333 <nikitosfox333@gmail.com>
2022-12-02 22:01:37 +0200

    finish task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+const itemsEl = [];
+
+ingredients.map(ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = ingredient;
+
+  return itemsEl.push(itemEl);
+});
+console.log(itemsEl);
+
+ingredientsEl.append(...itemsEl);
