29014c1f659248830ac0cc23626cf15fd49c2632
333Nikita333 <nikitosfox333@gmail.com>
2022-12-04 23:17:15 +0200

    finish task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,43 @@
+const refs = {
+  inputEl: document.querySelector('input'),
+  btnCreateBoxEl: document.querySelector('button[data-create]'),
+  btnDestroyBoxEl: document.querySelector('button[data-destroy]'),
+  boxСontainerEl: document.querySelector('#boxes'),
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount) {
+  let arrayBoxes = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const newBox = document.createElement('div');
+    newBox.style.backgroundColor = getRandomHexColor();
+    newBox.style.width = '${30 + 10 * i} px';
+    newBox.style.hight = '${30 + 10 * i} px';
+    arrayBoxes.push(newBox);
+  }
+
+  return arrayBoxes;
+}
+
+function destroyBoxes() {
+  refs.boxСontainerEl.innerHTML = '';
+}
+
+refs.btnCreateBoxEl.addEventListener('click', () => {
+  if (Number(refs.inputEl.value) === 0) {
+    return alert('Error! Please enter a valid value');
+  }
+
+  const newArrayBoxes = createBoxes(refs.inputEl.value);
+  console.log(newArrayBoxes);
+  refs.boxСontainerEl.append(...newArrayBoxes);
+});
+
+refs.btnDestroyBoxEl.addEventListener('click', () => {
+  destroyBoxes();
+  console.log(refs.boxСontainerEl);
+});
