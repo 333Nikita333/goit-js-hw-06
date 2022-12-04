61982d2d15866c2581d6f51ea526392de47c8fea
333Nikita333 <nikitosfox333@gmail.com>
2022-12-04 21:22:27 +0200

    finish task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const btnChangeColorEl = document.querySelector('.change-color');
+const colorEl = document.querySelector('.color');
+
+btnChangeColorEl.addEventListener('click', event => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorEl.innerHTML = bodyEl.style.backgroundColor;
+});
